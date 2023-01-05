50d22772331e50818b7852096af0a5fce71d7c92
khaledxab <khaledxab@gmail.com>
2023-01-05 21:01:03 +0100

    Adding CSS & Images

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,17 +1,20 @@
-import $ from "jquery";
-import _ from 'lodash';
+import '../css/main.css';
+const $ = require('jquery');
+const _ = require('lodash');
 
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the studnts</p>");
-$('body').append("<button>Click here to get started</button>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
-  
-function updateCounter() {
-  count = count + 1;
-  $('#count').text(`${count.toString()} clicks on the button`);
-}
+const updateCounter = () => {
+	let times = $('#count').html() || 0;
+	$('button').on('click', () => {
+		times++;
+		$('#count').html(`${times} clicks on the button`);
+	});
+};
 
-$('button').on('click', _.debounce(updateCounter));
+_.debounce(updateCounter, 500);
+updateCounter();
